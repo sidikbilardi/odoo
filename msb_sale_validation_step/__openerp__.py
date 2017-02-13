29672753abb85da524f0c82f1f867dd5778adfc7
Sidik Bilardi <sidik@willertindo.com>
2017-02-13 16:47:25 +0700

    message

---
+++ msb_sale_validation_step/__openerp__.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   Copyright (c) 2013 Agaplan (www.agaplan.eu)
+#   @author Peter Langenberg
+#
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+{
+    'name': 'Sales Order Validation Step',
+    'version': '1.0.0',
+    'category': '',
+    'description': """
+In some organisations people want an extra state between draft - send and confirmed.
+This module adds state validated to sale order, give an access to a certain people and puts also a menu extra in the sales
+    """,
+    'author': 'Sidik Bilardi',
+    'website': 'https://www.facebook.com/sidikbilardi',
+    'depends': ['sale_stock'],
+    'init': [],
+    'data': ['sale_view.xml','sale_workflow.xml','security/stock_security.xml'],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'active': False,
+    'price': 25.00,
+    'currency':'USD',
+}
---
+++ msb_sale_validation_step/sale.py
@@ -0,0 +1,63 @@
+##############################################################################
+#
+#    Agaplan 
+#    Copyright (C) 2004-2013 Agaplan NV (www.agaplan.eu)
+#    @author Peter Langenberg
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+from openerp.osv import osv, fields
+from openerp.tools.translate import _
+from openerp import netsvc
+
+class sale_order(osv.Model):
+    _inherit = "sale.order"
+    _description = "Validate quote"
+    _columns = {
+          'state': fields.selection([
+            ('draft', 'Draft Quotation'),
+            ('sent', 'Quotation Sent'),
+            ('cancel', 'Cancelled'),
+            ('validated','Validated'),
+            ('waiting_date', 'Waiting Schedule'),
+            ('progress', 'Sales Order'),
+            ('manual', 'Sale to Invoice'),
+            ('shipping_except', 'Shipping Exception'),
+            ('invoice_except', 'Invoice Exception'),
+            ('done', 'Done'),
+            ], 'Status', readonly=True,help="Gives the status of the quotation or sales order.\
+              \nThe exception status is automatically set when a cancel operation occurs \
+              in the invoice validation (Invoice Exception) or in the picking list process (Shipping Exception).\nThe 'Waiting Schedule' status is set when the invoice is confirmed\
+               but waiting for the scheduler to run on the order date.", select=True),
+     }
+
+    def action_button_validate(self, cr, uid, ids, context=None):
+        assert len(ids) == 1, 'This option should only be used for a single id at a time.'
+        wf_service = netsvc.LocalService('workflow')
+        wf_service.trg_validate(uid, 'sale.order', ids[0], 'order_validate', cr)
+
+        # redisplay the record as a sales order
+        view_ref = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'sale', 'view_order_form')
+        view_id = view_ref and view_ref[1] or False,
+        return {
+            'type': 'ir.actions.act_window',
+            'name': _('Sales Order'),
+            'res_model': 'sale.order',
+            'res_id': ids[0],
+            'view_type': 'form',
+            'view_mode': 'form',
+            'view_id': view_id,
+            'target': 'current',
+            'nodestroy': True,
+            }
